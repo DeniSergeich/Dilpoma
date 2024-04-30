cde270d754c98eefec0a00ac49fb8d4aa292430b
Denis <damned_wolf@mail.ru>
2024-05-01 00:35:09 +0300

    Тестовая логика букета

---
+++ diploma/src/main/java/ru/sergeich/diploma/repositoryes/BouquetRepository.java
@@ -0,0 +1,11 @@
+package ru.sergeich.diploma.repositoryes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.sergeich.diploma.domain.Bouquet;
+
+import java.util.Optional;
+@Repository
+public interface BouquetRepository extends JpaRepository<Bouquet, Long> {
+    Optional<Bouquet> findById(long id);
+}
