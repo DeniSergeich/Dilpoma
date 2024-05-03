1642ec8df4c44425eba9831d6432ac18348c263a
alexsavkaev <lesh1y174@gmail.com>
2024-05-03 16:12:56 +0300

    Исправил заполнение бд букетами 🤓

--- diploma/src/main/resources/data.sql
+++ diploma/src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO bouquets (id, name, count, price, description) VALUES
-    (1, 'Букет Микс', 10, 1500, 'Description 1'),
-    (2, 'Букет Комплимент', 20, 1800, 'Description 2'),
-    (3, 'Букет Свадебный', 30, 2300, 'Description 3'),
-    (4, 'Букет Джентельмен', 40, 5000, 'Description 4'),
-    (5, 'Букет Вьюга', 50, 3100, 'Description 5'),
-    (6, 'Букет Джунгли', 60, 2800, 'Description 6'),
-    (7, 'Букет Восход', 70, 1900, 'Description 7'),
-    (8, 'Букет Стихия', 80, 2600, 'Description 8'),
-    (9, 'Букет Удача', 90, 2400, 'Description 9');
-
+INSERT INTO bouquets (id, name, amount, price, description, image) VALUES
+(1, 'Букет Микс', 10, 1500.0, 'description', '/static/bouquet_1.jpg'),
+(2, 'Букет Комплимент', 10, 1800.0, 'description', '/static/bouquet_2.jpg'),
+(3, 'Букет Свадебный', 10, 2300.0, 'description', '/static/bouquet_3.jpg'),
+(4, 'Букет Джентельмен', 10, 5000.0, 'description', '/static/bouquet_4.jpg'),
+(5, 'Букет Вьюга', 10, 3100.0, 'description', '/static/bouquet_5.jpg'),
+(6, 'Букет Джунгли', 10, 2800.0, 'description', '/static/bouquet_6.jpg'),
+(7, 'Букет Восход', 10, 1900.0, 'description', '/static/bouquet_7.jpg'),
+(8, 'Букет Стихия', 10, 2600.0, 'description', '/static/bouquet_8.jpg'),
+(9, 'Букет Удача', 10, 2400.0, 'description', '/static/bouquet_9.jpg'),
+(10, 'Неизвестный букет', 10, 10.0, 'description', '/static/icon.jpg');
