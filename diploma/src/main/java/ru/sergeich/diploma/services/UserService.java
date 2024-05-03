bf14f83002d389273aff9382ea21f6b0fce8a8b2
DenniSergeich <122987383+DeniSergeich@users.noreply.github.com>
2024-05-03 19:35:53 +0300

    Дополнил UserController, UserService. Добавил RedirectController, RegisterController. Fix register.html

--- diploma/src/main/java/ru/sergeich/diploma/services/UserService.java
+++ diploma/src/main/java/ru/sergeich/diploma/services/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ru.sergeich.diploma.domain.User;
 import ru.sergeich.diploma.repositories.UserRepository;
@@ -12,6 +13,10 @@ import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
     private UserRepository userRepository;
 
@@ -22,5 +27,28 @@ public class UserService implements UserDetailsService {
 
     public Optional<User> getUserById(Long userId) {
         return userRepository.findById(userId);
+    }
+
+    public User findUserByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    public void rootResaveUser(User user){
+        userRepository.save(user);
+    }
+
+    public void rootResaveUserWithPassword(User user){
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public boolean saveUser(User user){
+        User userFromDB = userRepository.findByUsername(user.getUsername());
+        if (userFromDB != null){
+            return false;
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return true;
     }
 }
