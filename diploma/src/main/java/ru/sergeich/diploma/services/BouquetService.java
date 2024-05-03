f5010b002452dbba5f43f4f61b55c3dff7ed2595
alexsavkaev <lesh1y174@gmail.com>
2024-05-03 17:57:41 +0300

    CRUD в букетСервисе

--- diploma/src/main/java/ru/sergeich/diploma/services/BouquetService.java
+++ diploma/src/main/java/ru/sergeich/diploma/services/BouquetService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.sergeich.diploma.domain.Bouquet;
 import ru.sergeich.diploma.repositories.BouquetRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,8 +14,49 @@ public class BouquetService {
     @Autowired
     private BouquetRepository bouquetRepository;
 
+    /**
+     * Получение букета по айди
+     * @param bouquetId ид букета
+     * @return букет
+     */
     public Optional<Bouquet> getBouquetById(Long bouquetId) {
 
         return bouquetRepository.findById(bouquetId);
     }
+
+    /**
+     * Получение всех букетов
+     * @return список букетов
+     */
+    public List<Bouquet> getAllBouquets() {
+        return bouquetRepository.findAll();
+    }
+
+    /**
+     * Добавление букета
+     * @param bouquet букет
+     */
+
+    public void addBouquet(Bouquet bouquet) {
+        bouquetRepository.save(bouquet);
+    }
+
+    /**
+     * Обновить букет
+     * @param id ид букета для обновления
+     * @param updatedBouquet новый букет
+     */
+    public void updateBouquet(Long id, Bouquet updatedBouquet) {
+        bouquetRepository.save(updatedBouquet);
+    }
+
+    /**
+     * Удалить букет
+     * @param id ид букета
+     */
+    public void deleteBouquet(Long id) {
+        bouquetRepository.deleteById(id);
+    }
+
+
 }
