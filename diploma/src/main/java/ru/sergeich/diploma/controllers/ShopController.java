5ce0cd45cd7608929788fd14afe926afec76d621
Denis <damned_wolf@mail.ru>
2024-05-04 18:18:44 +0300

    Fix

--- diploma/src/main/java/ru/sergeich/diploma/controllers/ShopController.java
+++ diploma/src/main/java/ru/sergeich/diploma/controllers/ShopController.java
@@ -1,5 +1,6 @@
 package ru.sergeich.diploma.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,17 +8,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.sergeich.diploma.domain.Bouquet;
+import ru.sergeich.diploma.domain.Order;
 import ru.sergeich.diploma.services.BouquetService;
+import ru.sergeich.diploma.services.CartService;
+import ru.sergeich.diploma.services.UserService;
 
 import java.util.List;
 
 @Controller
 public class ShopController {
-    private final BouquetService bouquetService;
 
-    public ShopController(BouquetService bouquetService) {
-        this.bouquetService = bouquetService;
-    }
+    @Autowired
+    private BouquetService bouquetService;
+
+//    @Autowired
+//    private  UserService userService;
+
+//    public ShopController(BouquetService bouquetService) {
+//        this.bouquetService = bouquetService;
+//    }
 
     @GetMapping("/shop")
     public String getShop(Model model) {
