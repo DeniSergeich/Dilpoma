3a377f862d7d2a65848f04afc9b95b66647ba698
alexsavkaev <lesh1y174@gmail.com>
2024-05-04 18:42:31 +0300

    поправил data.sql и верстку карточек

--- diploma/src/main/resources/data.sql
+++ diploma/src/main/resources/data.sql
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS bouquets (
 );
 
 INSERT INTO bouquets (id, name, amount, price, description, image) VALUES
-(1, 'Букет Микс', 10, 1500.0, 'description', '/static/bouquet_1.jpg'),
-(2, 'Букет Комплимент', 10, 1800.0, 'description', '/static/bouquet_2.jpg'),
-(3, 'Букет Свадебный', 10, 2300.0, 'description', '/static/bouquet_3.jpg'),
-(4, 'Букет Джентельмен', 10, 5000.0, 'description', '/static/bouquet_4.jpg'),
-(5, 'Букет Вьюга', 10, 3100.0, 'description', '/static/bouquet_5.jpg'),
-(6, 'Букет Джунгли', 10, 2800.0, 'description', '/static/bouquet_6.jpg'),
-(7, 'Букет Восход', 10, 1900.0, 'description', '/static/bouquet_7.jpg'),
-(8, 'Букет Стихия', 10, 2600.0, 'description', '/static/bouquet_8.jpg'),
-(9, 'Букет Удача', 10, 2400.0, 'description', '/static/bouquet_9.jpg'),
-(10, 'Неизвестный букет', 10, 10.0, 'description', '/static/icon.jpg')
+(1, 'Букет Микс', 10, 1500.0, 'description', 'bouquet_1.jpg'),
+(2, 'Букет Комплимент', 10, 1800.0, 'description', 'bouquet_2.jpg'),
+(3, 'Букет Свадебный', 10, 2300.0, 'description', 'bouquet_3.jpg'),
+(4, 'Букет Джентельмен', 10, 5000.0, 'description', 'bouquet_4.jpg'),
+(5, 'Букет Вьюга', 10, 3100.0, 'description', 'bouquet_5.jpg'),
+(6, 'Букет Джунгли', 10, 2800.0, 'description', 'bouquet_6.jpg'),
+(7, 'Букет Восход', 10, 1900.0, 'description', 'bouquet_7.jpg'),
+(8, 'Букет Стихия', 10, 2600.0, 'description', 'bouquet_8.jpg'),
+(9, 'Букет Удача', 10, 2400.0, 'description', 'bouquet_9.jpg'),
+(10, 'Неизвестный букет', 10, 10.0, 'description', 'icon.jpg')
 ON CONFLICT DO NOTHING;
