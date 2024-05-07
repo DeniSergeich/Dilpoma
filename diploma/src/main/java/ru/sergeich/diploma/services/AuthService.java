f7db75f7b6af7310cb88568fd2cff50f89bc2aac
alexsavkaev <lesh1y174@gmail.com>
2024-05-07 03:33:54 +0300

    добавил некоторые комментарии в код

--- diploma/src/main/java/ru/sergeich/diploma/services/AuthService.java
+++ diploma/src/main/java/ru/sergeich/diploma/services/AuthService.java
@@ -3,17 +3,22 @@ package ru.sergeich.diploma.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import ru.sergeich.diploma.domain.User;
 import ru.sergeich.diploma.exceptions.UserNotFoundException;
-import ru.sergeich.diploma.repositories.UserRepository;
 
+/**
+ * Сервис для обновления авторизованного пользователя в контексте безопасности
+ */
 @Service
 public class AuthService {
     @Autowired
     UserService userService;
 
+    /**
+     * Обновляет пользователя в контексте безопасности
+     * @param user пользователь с обновленными данными
+     */
     public void updateAuthentication(User user) {
             // Создаем новый объект User с обновленными данными
             User updatedUser = userService.getUserById(user.getId()).orElseThrow(UserNotFoundException::new);
